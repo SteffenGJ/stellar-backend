4299af948f679007cb4b36472f2197d07556c1d3
Steffengj <103410399+SteffenGJ@users.noreply.github.com>
2023-01-21 12:58:05 +0100

    adding to ports

--- utils/config.js
+++ utils/config.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 
 const MONGODB_URI = process.env.MONGODB_URI
 const SECRET = process.env.SECRET
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 
 module.exports = {MONGODB_URI, SECRET, PORT};
