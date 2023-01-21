fde6772dd09c8e1f0a961da31fc16542b3402a80
Steffengj <103410399+SteffenGJ@users.noreply.github.com>
2023-01-21 12:34:33 +0100

    fixing ports

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const app = require("./app");
 const http = require("http");
+const {PORT} = require("./utils/config");
 
 const server = http.createServer(app);
 
-server.listen(3001, () => {
-    console.log("Server listening in port 3001..");
-})
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
--- utils/config.js
+++ utils/config.js
@@ -2,5 +2,6 @@ require("dotenv").config();
 
 const MONGODB_URI = process.env.MONGODB_URI
 const SECRET = process.env.SECRET
+const PORT = process.env.PORT
 
-module.exports = {MONGODB_URI, SECRET};
+module.exports = {MONGODB_URI, SECRET, PORT};
